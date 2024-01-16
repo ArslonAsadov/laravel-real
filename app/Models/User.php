2952b84c1041da30358b6f5affbf9d4f82217b25
Asadov Arslon <153752221+ArslonAsadov@users.noreply.github.com>
2024-01-16 05:30:28 +0500

    bazaga migret

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,12 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+
     protected $fillable = [
+        'role_id',
         'name',
         'email',
         'password',
@@ -42,4 +39,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function role()
+    {
+   return $this->belongsTo(Role::class);
+    }
 }
