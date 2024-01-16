aa21955d36377efa6d6c4fad8c231e36d57b0a83
Asadov Arslon <153752221+ArslonAsadov@users.noreply.github.com>
2024-01-16 05:54:32 +0500

    Aplication

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,8 +39,6 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-    public function role()
-    {
-   return $this->belongsTo(Role::class);
-    }
+    public function role(){ return $this->belongsTo(Role::class); }
+     public function application() { return $this->hasMany(Application::class);}
 }
