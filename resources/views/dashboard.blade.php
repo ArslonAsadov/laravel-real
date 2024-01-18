09d998728957ffd5988dd67a4d5253ae48770c38
Asadov Arslon <153752221+ArslonAsadov@users.noreply.github.com>
2024-01-18 06:49:38 +0500

    user ichi dezayin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,12 +4,6 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://unpkg.com/tailwindcss@1.4.6/dist/tailwind.min.css" rel="stylesheet">
-    </head>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -48,21 +42,18 @@
                             <div class='w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl'>
                                 <div class='max-w-md mx-auto space-y-6'>
 
-                                    <form action="" method="POST" enctype="multipart/form-data">
+                                    <form action="{{route('applications.store')}}" method="POST" enctype="multipart/form-data">
                                         @csrf
                                         <h2 class="text-2xl font-bold ">Submit your application</h2>
                                         <p class="my-4 opacity-70">Adipisicing elit. Quibusdam magnam sed ipsam deleniti debitis laboriosam praesentium dolorum doloremque beata.</p>
                                         <hr class="my-6">
 
-                                        <label class="uppercase text-sm font-bold opacity-70">Language</label>
-                                        <input type="text" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
-                                          <label class="uppercase text-sm font-bold opacity-70">Mesege</label>
-                                        <textarea a class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none"></textarea>
-
+                                        <label class="uppercase text-sm font-bold opacity-70">subject</label>
+                                        <input type="text" name="subject" required class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
+                                        <label class="uppercase text-sm font-bold opacity-70">Message</label>
+                                        <textarea name="message" required class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none"></textarea>
                                         <label class="uppercase text-sm font-bold opacity-70">file</label>
-                                        <input type="file" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
-
-
+                                        <input type="file" name="file" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
                                         <input type="submit" class="py-3 px-6 my-2 bg-emerald-500 text-danger font-medium rounded hover:bg-indigo-500  ease-in-out duration-300" value="Send">
                                     </form>
 
